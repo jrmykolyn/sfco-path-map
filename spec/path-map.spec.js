eb63fa6e2f3842bc7e6ef87aadc37be13be9f0f7
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-04-28 18:20:56 -0400

    Built out initial tests for 'PathMap'.

---
+++ spec/path-map.spec.js
@@ -0,0 +1,43 @@
+const PathMap = require( '../path-map' );
+
+describe( 'Test `PathMap`:', () => {
+	it( 'Should accept a configuration object with a single, "fully defined" path.', () => {
+		const PATHS = new PathMap( {
+			path1: './path-1'
+		} );
+
+		expect( PATHS.path1 ).toBe( './path-1' );
+	} );
+
+	it( 'Should accept a configuration object with multiple "fully defined" paths.', () => {
+		const PATHS = new PathMap( {
+			path1: './path-1',
+			path2: './path-2'
+		} );
+
+		expect( PATHS.path2 ).toBe( './path-2' );
+	} );
+
+	it( 'Should accept a configuration object which contains a mix of "fully defined" and "partially defined" paths.', () => {
+		const PATHS = new PathMap( {
+			path1: './path-1',
+			path2: './path-2',
+			path3: '__path1__/path-3'
+		} );
+
+		expect( PATHS.path3 ).toBe( './path-1/path-3' );
+	} );
+
+	it ( 'Should throw an error if the value for a given path is not of type "string"', () => {
+		try {
+			const PATHS = new PathMap( {
+				path1: './path-1',
+				path2: './path-2',
+				path3: '__path1__/path-3',
+				path4: true
+			} );
+		} catch ( err ) {
+			expect( err instanceof Error ).toBe( true );
+		}
+	} );
+} );
